57cad29c8da278e33e5f4d6233faa75f535f3ba0
Pandaphone <shawngreene075@gmail.com>
2022-08-21 00:50:20 -0400

    Done.

--- src/GithubThing/GithubCard.test.js
+++ src/GithubThing/GithubCard.test.js
@@ -1,10 +1,8 @@
-
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import gCard from './GithubCard';
 import renderer from 'react-test-renderer'
 
-
 test('renders a snapshot', () => {
-  const tree = renderer.create(<App/>).toJSON()
-  expect(tree).toMatchSnapshot()
-})
+    const tree = renderer.create(<gCard/>).toJSON()
+    expect(tree).toMatchSnapshot()
+  })
